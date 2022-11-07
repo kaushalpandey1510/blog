932b6305edc0093910465e8dfd28e9b761c9b615
Kaushal Pandey <kaushalpandey1510@gmail.com>
2022-11-07 00:00:43 -0500

    Refactoring of BlogContext to reusable context

--- src/Screen/IndexScreen.js
+++ src/Screen/IndexScreen.js
@@ -1,19 +1,16 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, FlatList } from 'react-native';
 import React, { useContext } from 'react';
-import BlogContext from '../context/BlogContext';
-import { FlatList } from 'react-native-gesture-handler';
+import { Context } from '../context/BlogContext';
 
 const IndexScreen = () => {
-  const { data, addBlogPost } = useContext(BlogContext);
+  const { state, addBlogPost } = useContext(Context);
   return (
     <View>
       <Text>Index Screen</Text>
       <Button title='Add Blog' onPress={addBlogPost} />
       <FlatList
-        data={data}
-        keyExtractor={(blogPost) => {
-          blogPost.title;
-        }}
+        data={state}
+        keyExtractor={(blogPost) => blogPost.title}
         renderItem={({ item }) => {
           return <Text>{item.title}</Text>;
         }}
@@ -22,6 +19,6 @@ const IndexScreen = () => {
   );
 };
 
-export default IndexScreen;
-
 const styles = StyleSheet.create({});
+
+export default IndexScreen;
