2854ad8fc9cfab8afe9d9b884c56d0acacd7b147
Kaushal Pandey <kaushalpandey1510@gmail.com>
2022-11-09 19:02:58 -0500

    minor css updates to ShowScreen

--- src/Screen/ShowScreen.js
+++ src/Screen/ShowScreen.js
@@ -11,9 +11,9 @@ const ShowScreen = ({ navigation }) => {
   );
 
   return (
-    <View>
-      <Text>{blogPost.title}</Text>
-      <Text>{blogPost.content}</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>{blogPost.title}</Text>
+      <Text style={styles.content}>{blogPost.content}</Text>
     </View>
   );
 };
@@ -26,7 +26,7 @@ ShowScreen.navigationOptions = ({ navigation }) => {
           navigation.navigate('Edit', { id: navigation.getParam('id') })
         }
       >
-        <FontAwesome name='pencil' size={32} color='black' />
+        <FontAwesome style={styles.editIcon} name='pencil' color='black' />
       </TouchableOpacity>
     ),
   };
@@ -34,4 +34,21 @@ ShowScreen.navigationOptions = ({ navigation }) => {
 
 export default ShowScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    margin: 20,
+    padding: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  content: {
+    marginTop: 20,
+    fontSize: 18,
+  },
+  editIcon: {
+    fontSize: 32,
+    marginRight: 10,
+  },
+});
